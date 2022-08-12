7343bf09dabf528c70ba5126b75aa65ff00d4d52
Peru PC <paulocondori1@gmail.com>
2022-08-11 21:01:14 -0400

    first draft

---
+++ models/Category.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    Category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+    // define columns
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
